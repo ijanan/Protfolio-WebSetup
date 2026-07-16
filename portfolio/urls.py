10963922b6be776e2de38c09dcf2a36b2ddb6815
ISTIAK JAMAN ANAN <93780297+ijanan@users.noreply.github.com>
2026-07-17 00:29:29 +0600

    Serve uploaded media in production (fix images 404 on PythonAnywhere)

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -17,11 +17,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve uploaded media (profile images, project images, resume).
+# Required in production (DEBUG=False) on PythonAnywhere because Django
+# does not serve media files itself.
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
+]
